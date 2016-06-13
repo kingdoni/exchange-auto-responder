77eb341daca3e315caa52d9034fa31d1180cfed9
kingdoni <donald.wanie@hotmail.com>
2016-06-13 12:12:03 +0100

    Add AutoRepyAction Class

---
+++ G.Exchange.Autoresponder.Agent/Actions/AutoReplyAction.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rules.Framework.Rules;
+
+namespace G.Exchange.Autoresponder.Agent.Actions
+{
+    [Serializable]
+    public class AutoReplyAction : RuleAction
+    {
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(object o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
